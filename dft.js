48efa03491c2509865a23c1b7de3e1b3546f54b4
YutaOgata <68010913+YutaOgata@users.noreply.github.com>
2021-07-22 11:31:33 +0900

    Update dft.js

--- dft.js
+++ dft.js
@@ -1,27 +1,37 @@
-/**class dft {
+class dft {
   
-  constructor( elm0, elm1, elm2, elm3 ) {
-    this.elm0;
-    this.elm1;
-    this.elm2;
-    this.elm3;
+  constructor( elm1, elm2, elm3 ) {
+    this.elm1 = elm1;
+    this.elm2 = elm2;
+    this.elm3 = elm3;
+    
+    vc1 = elm1;
+    ctx1 = vc1.getContext('2d');
+    
+    ctx1.strokeStyle ='red';
+    ctx1.lineWidth = 2;
+    ctx1.beginPath();
+    ctx1.moveTo(0,250); //始点
+ 
+    for(var x=1; x<=canvas.width; x +=1) {
+       var y =-r*Math.sin((2*Math.PI/T)*x*2); //振幅 * Math.sin( 角速度(2π/周期)*時間 )
+       ctx1.lineTo(x, y+(canvas.height/2));
+     }
+  
+    ctx1.stroke();
   }
 }
 
 window.addEventListener("load", function(e) {
-  guisetup();
 
   var x = new dft(
-    document.querySlector('graph1'),
-    document.querySlector('graph1b'),
-    document.querySlector('graph2'),
-    document.querySlector('graph2b'),
-    document.querySlector('theory'),
-    document.querySlector('answer')
+    document.querySlector('#canvas1'),
+    document.querySlector('#canvas2'),
+    document.querySlector('#result'),
    );
 
 });
-**/
+/**
 window.addEventListener('load',draw,false);
 
 function draw(){
