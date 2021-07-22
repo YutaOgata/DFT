1dc1e8e062b9fb3a287063fe3ac4ae083bea4ae8
YutaOgata <68010913+YutaOgata@users.noreply.github.com>
2021-07-22 12:39:13 +0900

    Update dft.js

--- dft.js
+++ dft.js
@@ -5,17 +5,17 @@ function draw(){
   var r = 100;
   var T = 600;
   
-  var canvas = document.querySlector('#canvas1');
-  var ctx = canvas1.getContext('2d');
+  var canvas = document.querySelector('#canvas1');
+  var ctx = canvas.getContext('2d');
     
   ctx.strokeStyle ='red';
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(0,250); //始点
  
-  for(var x=1; x<=canvas1.width; x +=1) {
+  for(var x=1; x<=canvas.width; x +=1) {
       var y =-r*Math.sin((2*Math.PI/T)*x*2); //振幅 * Math.sin( 角速度(2π/周期)*時間 )
-      ctx.lineTo(x, y+(canvas1.height/2));
+      ctx.lineTo(x, y+(canvas.height/2));
     }
   
   ctx.stroke();
